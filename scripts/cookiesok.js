670beecdb65957c7895c11a97aaf5b443702012c
Nick Sulkers <nicksulkers@beachconcepts.nl>
2016-02-16 12:54:26 +0100

    remove log method and all uses of it

--- scripts/cookiesok.js
+++ scripts/cookiesok.js
@@ -4,28 +4,19 @@
 	var retryTimeout = 500;
 	var attemptsLimit = 10;
 
-	function log(obj){
-		//console.log(obj);
-	}
-
 	function performOrder(orders, attempt){
 		if(!attempt)
 			attempt = 1;
 
-		log('performOrder, attempt nr.: ' + attempt + '');
-
 		var frames = orders.target.split("->");
-		log('Searching target DOM: document.querySelector("' + frames[0] + '")');
 		var target = document.querySelector(frames[0]);
 
-		for(var i = 1; target && i < frames.length; ++i){
+		for(var i = 1; target && i < frames.length; ++i)
 			try{
-				log('.querySelector("' + frames[i] + '")');
 				target = target.contentWindow.document.querySelector(frames[i])
 			}catch(ex){
 				target = null;
 			}
-		}
 
 		if(!target){
 			if(attempt < attemptsLimit)
@@ -33,7 +24,6 @@
 			return;
 		}
 
-		log('target found! Performing ' + orders.action);
 		switch(orders.action){
 			case 'hide':
 				target.style.display = 'none';
@@ -49,7 +39,6 @@
 
 	//Always Attempt to execute the CookiesOK method, remove from source after execution
 	//this allows websites to recognize CookiesOK and assume consent
-	log('Attempting execution of JS function CookiesOK("' + version + '")');
 	var script = document.createElement('script');
 	script.innerHTML = 'if(window.CookiesOK) window.CookiesOK("' + version + '");';
 	var head = document.getElementsByTagName("head")[0];
@@ -63,16 +52,12 @@
 		if(!getDomainOrdersComplete) //this should never happen.. but in theory, it could
 			return setTimeout(performOrders, 15);
 
-		log('Looking up ' + location.hostname + ' in database');
 		if(orders){
-			log("Orders found...")
 			if(orders.action)
 				orders = [orders];
 
 			for(var i in orders)
 				performOrder(orders[i]);
-		}else{
-			log("No orders found...")
 		}
 
 		for(var o in hideStyles)
